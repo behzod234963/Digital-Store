51ddec3e9e7f66e5b472528cfa7a5e4ab3b77289
Behzod96447 <behzod234963@internet.ru>
2023-09-16 12:33:41 +0500

    Digital-Store

--- app/src/main/java/com/example/digital_store/Fragments/Main/SignUpStepOne.kt
+++ app/src/main/java/com/example/digital_store/Fragments/Main/SignUpStepOne.kt
@@ -7,14 +7,14 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.digital_store.R
 
-class SignUp : Fragment() {
+class SignUpStepOne : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+        return inflater.inflate(R.layout.fragment_sign_up_step_one, container, false)
     }
 
 
